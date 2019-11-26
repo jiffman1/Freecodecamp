9109a0565bba465dcacbc60758079e2adb1f4847
jiffman1 <chongomum@yahoo.com>
2019-11-26 01:38:05 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -196,22 +196,15 @@ app.use(helmet.noCache());
 // ---- DO NOT EDIT BELOW THIS LINE ---------------------------------------
 
 module.exports = app;
-var api = require("./server.js");
-app.use(express.static("public"));
-app.disable("strict-transport-security");
-app.use("/_api", api);
+var api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
 
 app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + '/views/index.html');
 });
 var listener = app.listen(process.env.PORT || 3000, function() {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", 'trusted-cdn.com']
-    }
-  })
-)
+add app.use(helmet.contentSecurityPolicy({ directives: { defaultSrc: ["'self'"], scriptSrc: ["'self'", 'trusted-cdn.com'] }} ))
